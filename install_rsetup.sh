acb303b56b59750f1ee3515660771436060e5159
Ross Nelson <ross.nelson@gmail.com>
2015-03-01 20:39:23 -0600

    Create /var/db/rsetup during the install

--- install_rsetup.sh
+++ install_rsetup.sh
@@ -7,8 +7,10 @@ sudo pkg install -y git curl wget ca_root_nss ezjail >/dev/null
 sudo ln -s /usr/local/etc/ssl/cert.pem /etc/ssl/cert.pem
 echo "Installing rsetup"
 sudo mkdir /usr/rsetup
+sudo mkdir -p /var/db/rsetup
 sudo git clone https://github.com/rnelson/rsetup.git /usr/rsetup >/dev/null
 (cd /usr/rsetup; sudo git checkout freebsd-10.1) >/dev/null
 sudo ln -s /usr/rsetup/rsetup /usr/local/bin/rsetup
-sudo chown -R vagrant:vagrant /usr/rsetup
+sudo chown -R `id -u`:`id -g` /usr/rsetup
+sudo chown -R `id -u`:`id -g` /var/db/rsetup
 date +%V > /var/db/rsetup/lastupdate
